796cdd16c149548d3cc2b75a78ee3cffaf5d81e6
kuniaki <kuniaki@iMac.local>
2021-12-31 13:48:50 +0900

    test2

--- app/src/server.py
+++ app/src/server.py
@@ -14,6 +14,10 @@ app = Flask('app server')
 @app.route('/stock', methods=['GET'])
 def stock():
   df = {}
+  code    = request.args.get('code', default="7974", type=str)
+  country = request.args.get('country', default="japan", type=str)
+  from    = request.args.get('from', type=str)
+  to      = request.args.get('from', type=str)
   return success(df)
 """
   code    = request.args.get('code', default="7974", type=str)
