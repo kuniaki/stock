a6954b2a1b4c2624851b93757865a38cc88ec346
kuniaki <kuniaki@iMac.local>
2021-12-31 16:50:26 +0900

    test

--- app/src/server.py
+++ app/src/server.py
@@ -23,6 +23,10 @@ def root():
 @app.route('/stock')
 def stock():
   df = "aaa"
+  df = investpy.get_stock_historical_data(stock='7974',
+                                        country='japan',
+                                        from_date='01/01/2020',
+                                        to_date='01/01/2021')
   return success(df)
 """
 # code      = request.args.get('code')
