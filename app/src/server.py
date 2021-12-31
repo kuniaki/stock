df00e5b0e659827f983f95b03c3e4f2f5c980a49
kuniaki <kuniaki@iMac.local>
2021-12-31 15:53:44 +0900

    test

--- app/src/server.py
+++ app/src/server.py
@@ -28,12 +28,17 @@ def stock():
 # fromDate  = request.args.get('fromDate')
 # toDate    = request.args.get('toDate')
 
+  df = investpy.get_stock_recent_data(
+    stock='7974',
+    country='japan'
+  )
+
+"""
   df = investpy.get_stock_historical_data(stock='7974',
                                         country='japan',
                                         from_date='01/01/2020',
                                         to_date='01/01/2021')
 
-"""
   df = investpy.get_stock_historical_data(stock=code,
                                         country=country,
                                         from_date=fromDate,
