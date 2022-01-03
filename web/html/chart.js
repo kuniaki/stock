faae261aa207dd0ea75dab87d81a581cd295d5bd
kuniaki <kuniaki@iMac.local>
2022-01-03 12:27:22 +0900

    fixed error

--- web/html/chart.js
+++ web/html/chart.js
@@ -11,7 +11,7 @@ $('#btn').click(function(){
 
 function getInfo(code, span,dates,datee,countryc,callback){
     $.ajax({
-        url : 'http://kuniakistock.westus.cloudapp.azure.com/api/v1/stock?code=' + code + '&country='+ countryc  + '&from_date=' + dates + '&to_date=' + datee,
+        url : 'http://http://192.168.11.60/api/v1/stock?code=' + code + '&country='+ countryc  + '&from_date=' + dates + '&to_date=' + datee,
         type : 'GET',       
         async : true,        
         cashe : false,     
