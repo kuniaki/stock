66f99b20c82655aba4c95dbec458bb2f1062d425
kuniaki <kuniaki@iMac.local>
2022-01-17 13:47:49 +0900

    delete average

--- web/html/chart.js
+++ web/html/chart.js
@@ -46,57 +46,7 @@ function mainChart(result){
         volume_d = result["volume"]
         //描画用のデータを一時的に入れる
         var insertingData = new Array(length);
-        //平均を出すための割り算の分母
-        var divide = 0;
-        //二次元配列aveに、平均線の日数と平均値を入れる
-        var ave = new Array();
-        //５日平均線用
-        ave[0] = new Array();
-        //25日平均線用
-        ave[1] = new Array();
-        //50日平均線用
-        ave[2] = new Array();
-        //平均線の計算に用いる
-        var temp = 0;
-        //５日移動平均線の算出
-        //基準日より５日前までのデータを足し合わせ、平均値を出す
-        for(var m = 0; m < length - 4; m++){
-            for(var n = 0; n < 5; n++){
-                if(close_d[m+n] != ''){
-                    temp = temp + parseFloat(close_d[m+n]);
-                    divide++;
-                }
-            }
-            ave[0][m] = temp / divide;
-            temp = 0;
-            divide = 0;
-        }
-        //2５日移動平均線の算出
-        //上と同様の処理
-        for(var m = 0; m < length - 24; m++){
-            for(var n = 0; n < 25; n++){
-                if(close_d[m+n] != ''){
-                    temp = temp + parseFloat(close_d[m+n]);
-                    divide++
-                }
-            }
-            ave[1][m] = temp / divide;
-            temp = 0;
-            divide = 0;
-        }
-        //５0日移動平均線の算出
-        //上と同様の処理
-        for(var m = 0; m < length - 49; m++){
-            for(var n = 0; n < 49; n++){
-                if(close_d[m+n] != ''){
-                    temp = temp + parseFloat(close_d[m+n]);
-                    divide++
-                }
-            }
-            ave[2][m] = temp / divide;
-            temp = 0;
-            divide = 0;
-        }
+
         //for文をまとめるため、出来高棒グラフの処理もここで行う
         //出来高を保持する配列
         var volume = new Array();
@@ -108,13 +58,13 @@ function mainChart(result){
                 dates[s] = String(date_d[s]);
             }
         }
-        //配列insertingDataの中に、[安値、始値、高値、終値、５日移動平均線、２５日移動平均線、５０日移動平均線]の形で値を入れ込む
+        //配列insertingDataの中に、[安値、始値、高値、終値]の形で値を入れ込む
         for(var a = 0; a < length; a++){
-            insertingData[a] = [dates[a],parseFloat(low_d[a]),parseFloat(open_d[a]),parseFloat(close_d[a]),parseFloat(high_d[a]),ave[0][a],ave[1][a],ave[2][a]]
+            insertingData[a] = [dates[a],parseFloat(low_d[a]),parseFloat(open_d[a]),parseFloat(close_d[a]),parseFloat(high_d[a])]
         }
         //チャート描画用の配列の中に、insertingDataの値を入れ込む
         //最古の50日分のデータまでは移動平均線のデータが揃っていないので、取り除く
-        for (var i = insertingData.length - 50; i > 0; i--){
+        for (var i = insertingData.length; i > 0; i--){
             chartData.addRow(insertingData[i]);
         }
         //チャートの見た目に関する記述、詳細は公式ドキュメントをご覧になってください
@@ -174,7 +124,7 @@ function volumeChart(volume, dates, length){
         insertingData[a] = [dates[a],parseInt(volume[a])]
     }
     //insertingDataの値をチャート描画用の変数に入れ込む
-    for (var i = insertingData.length - 50; i > 0; i--){
+    for (var i = insertingData.length; i > 0; i--){
         chartData.addRow(insertingData[i]);
     }
     //ローソク足の時と同じように、見た目の設定をする
