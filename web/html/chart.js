11258e5000091d10a8fa9ab507cb328ce88acc21
kuniaki <kuniaki@iMac.local>
2022-01-18 05:14:35 +0900

    fix graph

--- web/html/chart.js
+++ web/html/chart.js
@@ -111,7 +111,7 @@ function mainChart(result){
                        ,{ type: "line" }
                     ]
                     ,vAxes: [
-                        { title: "%",viewWindowMode:'maximized'}
+                        { title: "%",,minValue:-20,maxValue:80 }
                    ]
                    ,hAxis: [{
                          format: 'yy/MM/dd',
