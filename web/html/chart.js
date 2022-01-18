a149fdebdc67ef0e75a005b222623abaf919dd75
kuniaki <kuniaki@iMac.local>
2022-01-18 11:08:23 +0900

    delete unnecessary input text

--- web/html/chart.js
+++ web/html/chart.js
@@ -30,6 +30,54 @@ function getInfo(code,dates,datee,countryc,callback){
         console.log(result)
     });  
 }
+function main1years(result) {
+    var chartData = new google.visualization.DataTable();
+        chartData.addColumn("string","day");
+        chartData.addColumn("number","2502.T");
+        chartData.addColumn("number","N225");
+        date_d = result["date"]
+        var length = date_d.length;
+        close_d = result["close"]
+        nikkei_d = result["nikkei"]
+        var insertingData = new Array(length);
+
+        var dates = new Array();
+        for(var s = 0; s < length; s++){
+                dates[s] = String(date_d[s]);
+        }
+
+        for(var a = 0; a < length; a++){
+            insertingData[a] = [dates[a],parseFloat(close_d[a]),parseFloat(nikkei_d[a])]
+        }
+        for (var i = insertingData.length-1; i > insertingData.length -366; i--){
+            ttt = insertingData[i];
+            chartData.addRow(insertingData[i]);
+        }
+       
+        title = "期間:1年間"
+        stepSize = 1000
+
+        var options = {
+            title: title,
+            bar: { 
+                groupWidth: '100%' 
+            },
+            hAxis: {
+                stepSize:stepSize,
+                format: 'yy/MM/dd',
+                direction: -1,
+            },
+            width: 1200,
+            height: 400,
+            lineWidth: 1,
+            curveType: 'function',
+            seriesType: "Line",  
+        };
+        //描画の処理
+        var chart = new google.visualization.ComboChart(document.getElementById('appendMain'));
+        chart.draw(chartData, options);
+
+}
  
 function main5years(result) {
     var chartData = new google.visualization.DataTable();
@@ -82,6 +130,7 @@ function main5years(result) {
 
 function mainChart(result){
    main5years(result)
+   main1years(result)
 }
 
 function volumeChart(volume, dates, length){
