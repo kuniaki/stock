99bd95407de12ed2c120efefae0007a42aeda1b2
kuniaki <kuniaki@iMac.local>
2022-01-24 13:22:05 +0900

    more clean up

--- web/html/event.js
+++ web/html/event.js
@@ -1,16 +1,6 @@
 $('#btn-getinfo').click(function(){
-    var code = $('#code').val()
-    var countryc =  $('#country').val()
 
-    date = new Date();
-    year = date.getFullYear();
-    month = date.getMonth() + 1;
-    day = date.getDate();
-    datee = day + '/' + month + '/' + year
-    year5 = date.getFullYear() - 5;
-    day1 = date.getDate()+1;
-    dates = day1 + '/' + month + '/' + year5
-    getInfo(code,dates,datee,countryc,mainChart);
-
+    getStock();
     getRSegmentInfo();
+
 })
