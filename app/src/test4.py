b2cbac947ad70e44da48d0f0ec8b87f5b5d101e6
kuniaki <kuniaki.kudo@gmail.com>
2022-01-26 07:35:24 +0900

    Update test4.py
    FIxed typo

--- app/src/test4.py
+++ app/src/test4.py
@@ -3,7 +3,7 @@ import json
 import pandas as pd
 
 
-code = '2502' #トヨタ自動車
+code = '2502' #Asahi
 annual = investpy.get_stock_financial_summary(stock=code, country='japan', summary_type='income_statement', period='annual')
 
 print(annual)
