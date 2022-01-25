dc4f3e875f958bb286a33b31fff7fcb4ecbea66e
Victoria Xu <victoriaxu@Victorias-MacBook-Air.local>
2022-01-25 16:11:48 +0900

    fixed - refresh div after click

--- web/html/event.js
+++ web/html/event.js
@@ -1,7 +1,13 @@
 $('#btn-getinfo').click(function () {
-
+    refreshDiv();
     getStock();
     if ($("#code").val() == 2502) {
         getRSegmentInfo();
     }
 })
+
+function refreshDiv() {
+    $("#append1year").load(window.location.href + " #append1year");
+    $("#append5year").load(window.location.href + " #append5year");
+    $("#rsegment").load(window.location.href + " #rsegment");
+}
