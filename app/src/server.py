141d7c9182e42ff6e135ea934c19e40c2c324d3f
Victoria Xu <victoriaxu@Victorias-MacBook-Air.local>
2022-01-27 15:39:16 +0900

    test

--- app/src/server.py
+++ app/src/server.py
@@ -38,7 +38,7 @@ def api_revenue():
   code  = request.args.get('code')
   country = request.args.get('country')
 
-  annual_revenue = investpy.get_stock_financial_summary(stock='2502', country='japan', summary_type='income_statement', period='annual')
+  annual_revenue = investpy.get_stock_financial_summary(stock=code, country=country, summary_type='income_statement', period='annual')
   revenue = annual_revenue.reset_index()
 
   dc = dict(date=[i for i in revenue['Date'].dt.date], total_revenue=[i for i in revenue['Total Revenue']], gross_profit=[i for i in revenue['Gross Profit']], operating_income=[i for i in revenue['Operating Income']], net_income=[i for i in revenue['Net Income']])
