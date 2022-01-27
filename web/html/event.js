2ce5c486f1b833b4496ec26bd0d3a83cfba6a03f
Victoria Xu <victoriaxu@Victorias-MacBook-Air.local>
2022-01-27 15:01:49 +0900

    test graph

--- web/html/event.js
+++ web/html/event.js
@@ -11,5 +11,5 @@ function refreshDiv() {
     $("#append1year").load(window.location.href + " #append1year");
     $("#append5year").load(window.location.href + " #append5year");
     $("#rsegment").load(window.location.href + " #rsegment");
-    $("#annual-revenue").load(window.location.href + "#annual-revenue");
+    // $("#annual-revenue").load(window.location.href + "#annual-revenue");
 }
--- web/html/revenue.js
+++ web/html/revenue.js
@@ -6,7 +6,6 @@ function getRevenueInfo() {
 
     $.ajax({
         url: `//www.jenkins-asahi.com/api/v1/revenue?code=${code}&country=${countryc}`,
-        // url: '//www.jenkins-asahi.com/api/v1/revenue',
         type: 'GET',
         async: true,
         cashe: false,
@@ -21,5 +20,33 @@ function getRevenueInfo() {
 }
 
 function revenueChart(result) {
-    alert('on the right track!');
+    console.log('on the right track!');
+    var chartData = new google.visualization.DataTable();
+
+    chartData.addColumn("string", "Date");
+    chartData.addColumn("number", "Total Revenue");
+    chartData.addColumn("number", "Gross Profit");
+    chartData.addColumn("number", "Operating Income");
+    chartData.addColumn("number", "Net Income");
+
+    for (let i = 0; i < result['date'].length; i++) {
+        chartData.addRow(list(result['date'][i], parseInt(result['total_revenue'][i]), parseInt(result['gross_profit'][i]), parseInt(result['operating_income'][i]), parseInt(result['net_income'][i])));
+    }
+
+    var options = {
+        title: 'Annual Revenue Chart',
+        bar: {
+            groupWidth: '75%'
+        },
+        hAxis: {
+            title: '年度',
+            direction: -1,
+        },
+        isStacked: false,
+        width: 600,
+        height: 400,
+    }
+
+    var chart = new google.visualization.ColumnChart(document.getElementById('revenue'));
+    chart.draw(chartData, options);
 }
