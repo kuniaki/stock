46a19088d73b8ea643b8d3b3de4436c7469392dc
Victoria Xu <victoriaxu@Victorias-MacBook-Air.local>
2022-02-02 16:39:53 +0900

    refresh div by adding css hidden class

--- web/html/event.js
+++ web/html/event.js
@@ -1,17 +1,32 @@
 google.charts.load("current", { packages: ["corechart"] });
 
 $("#btn-getinfo").click(function () {
-  refreshDiv();
+  // refreshDiv();
   console.log("running stock graph...");
   getStock();
   if ($("#code").val() == 2502) {
     console.log("running segment graph...");
     getRSegmentInfo();
+    addSegment();
+  } else {
+    removeSegment();
   }
   getRevenueInfo();
 });
 
-function refreshDiv() {
-  console.log("refreshing segment graph...");
-  $("#rsegment").load(window.location.href + " #rsegment");
+// function refreshDiv() {
+//   console.log("refreshing segment graph...");
+//   $("#rsegment").load(window.location.href + " #rsegment");
+// }
+
+function addSegment() {
+  console.log("...segment graph added...");
+  let hideSegment = document.querySelector(".hidden");
+  hideSegment.classList.remove("hidden");
+}
+
+function removeSegment() {
+  console.log("...segment graph removed...");
+  let hideSegment = document.querySelector(".hidden");
+  hideSegment.classList.add("hidden");
 }
