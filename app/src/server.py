8013c2e9f4888b3660905f2907cf12e80f6a7943
kuniaki <kuniaki@iMac.local>
2022-02-21 14:17:36 +0900

    removed null

--- app/src/server.py
+++ app/src/server.py
@@ -127,7 +127,7 @@ def api_stock():
        else:
            n.append(vv)
  
-  dc = dict(date=dates,close=close_d,nikkei=[i for i in n if i != ''])
+  dc = dict(date=dates,close=close_d,nikkei=[i for i in n if i != 'null'])
 # dc = dict(date=dates,close=close_d,nikkei=n,checksize=list(len(dates) - len(n)))
 
   return success(dc)
