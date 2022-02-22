b6f87761fc88ef07dae6fcb3ddd9e06cbd481cb5
kuniaki <kuniaki.kudo@gmail.com>
2022-02-22 22:04:43 +0900

    Update server.py

--- app/src/server.py
+++ app/src/server.py
@@ -94,10 +94,10 @@ def api_rsegment():
 @app.route('/api/v1/stock/',methods=['GET'])
 def api_stock():
   code  = request.args.get('code') 
-  result-code   = kabuka(code,5,1)
-  result-nikkei = kabuka(1320,5,1)
+  result1   = kabuka(code,5,1)
+  result0 = kabuka(1320,5,1)
 
-  dc = dict(date=result-code[3].keys(),close=result-code[3].values(),nikkei=result-nikkei[3].values())
+  dc = dict(date=result0[3].keys(),close=result1[3].values(),nikkei=result0[3].values())
 # dc = dict(date=dates,close=close_d,nikkei=n,checksize=list(len(dates) - len(n)))
 # dc = dict(date=dates,close=result[3],nikkei=result[3])
 
