e950287cb7bb21e30f3cf126a95986cc728c0042
Victoria Xu <yuxiaox@uci.edu>
2022-02-24 17:33:00 +0900

    test

--- web/html/info.js
+++ web/html/info.js
@@ -17,21 +17,22 @@ function getCompanyInfoPromise() {
 
 const infoPromiseDone = function (result) {
   console.log("loading company info...");
-  fillInTable(result, code);
+  fillInOverview(result["overview"], code);
 };
 
-function fillInTable(result, code) {
+function fillInOverview(info, code) {
+  console.log();
   const rowHeader = document.querySelector("#company-name-code");
   const row1col1 = document.querySelector(".row-1.col-1");
   const row2col1 = document.querySelector(".row-2.col-1");
   const row3col1 = document.querySelector(".row-3.col-1");
   const row4col1 = document.querySelector(".row-4.col-1");
 
-  rowHeader.textContent = `${code} ${result["会社情報"]}\n${result["英語社名"]}`;
-  row1col1.textContent = result["概要"];
-  row2col1.textContent = result["業種"];
-  row3col1.textContent = result["テーマ"];
-  row4col1.textContent = result["会社サイト"];
+  rowHeader.textContent = `${code} ${info["会社情報"]}\n${info["英語社名"]}`;
+  row1col1.textContent = info["概要"];
+  row2col1.textContent = info["業種"];
+  row3col1.textContent = info["テーマ"];
+  row4col1.textContent = info["会社サイト"];
 
   show(".company-info");
 }
