08568397db0046f27aca87baf8afb1533983be97
Victoria Xu <yuxiaox@uci.edu>
2022-03-08 20:31:50 +0900

    test form

--- web/html/select.js
+++ web/html/select.js
@@ -1,15 +1,15 @@
-document
-  .querySelector("news-delete-button")
-  .addEventListener("click", function (e) {
-    e.preventDefault();
-    let trace = 0;
-    for (let row of document.querySelectorAll(".check")) {
-      if (row.checked === true) {
-        deleteTableRow("news-table", trace === 0 ? 0 : trace--);
-      }
-      trace++;
+const newsDeleteButton = document.getElementById("news-delete-button");
+
+newsDeleteButton.addEventListener("click", function (e) {
+  e.preventDefault();
+  let trace = 0;
+  for (let row of document.querySelectorAll(".check")) {
+    if (row.checked === true) {
+      deleteTableRow("news-table", trace === 0 ? 0 : trace--);
     }
-  });
+    trace++;
+  }
+});
 
 function deleteTableRow(tableid, rowNum) {
   const table = document.getElementById(tableid);
