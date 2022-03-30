0f5792ea57ea00ab90dbc2c5bfbf6806364e3a5b
ASAHI-GH22001 <asahi-gh22001@ASAHI-GH22001s-MacBook-Pro.local>
2022-03-30 18:47:00 +0900

    test - stock revenue graph css

--- web/html/revenue.js
+++ web/html/revenue.js
@@ -23,21 +23,15 @@ function addRevenueChart(result, term, graph) {
   var chartData = new google.visualization.DataTable();
 
   chartData.addColumn("string", "Date");
-  chartData.addColumn("number", "売上"); // total revenue
+  chartData.addColumn("number", "売上(単位：億円)"); // total revenue
   chartData.addColumn("number", "%売上総利益"); // gross profit
   chartData.addColumn("number", "%営業利益"); // operating income
   chartData.addColumn("number", "%純利益"); // net income
-  // chartData.addColumn("number", "Total Revenue %"); // total revenue %
-
 
   for (let i = 0; i < result[term]["date"].length; i++) {
     chartData.addRow([
       result[term]["date"][i],
       parseInt(result[term]["total_revenue"][i]) / 100, // index 0
-      // parseInt(result[term]["gross_profit"][i]), // index 1
-      // parseInt(result[term]["operating_income"][i]), // index 2
-      // parseInt(result[term]["net_income"][i]), // index 3
-      // parseFloat(result[term]["revenue_percentage"][i]), // index 4
       parseFloat(result[term]["gross_percentage"][i]), // index 1
       parseFloat(result[term]["operating_percentage"][i]), // index 2
       parseFloat(result[term]["net_percentage"][i]), // index 3
@@ -78,7 +72,7 @@ function addRevenueChart(result, term, graph) {
       0: {
         // title: "Revenue",
         gridlines: {color: 'transparent'},
-        format: "¥###,###,###,###,###",
+        format: "###,###,###,###,###",
       },
       1: {
         // title: "Percentage",
